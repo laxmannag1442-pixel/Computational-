760ac55bce5471c4b6f35625ae3c428daadac5e2
laxmannag1442-pixel <laxmannag1442@gmail.com>
2025-08-14 15:18:39 +0530

    Update lib.py

--- laxman/lib.py
+++ laxman/lib.py
@@ -1,7 +1,23 @@
 class laxmanlibrary:
+  # matrices multiplication and vector dot product
+  def read_matrix(self,filename):
+      with open(filename,'r') as file:
+        matrix = []
+        for line in file:
+        # Split the line into numbers and convert to int/float
+          row =[float(num)for num in line.strip().split()]
+          matrix.append(row)
+        return matrix
+        
   def lcg(self,seed):
-    a =
-    c =
-    m =
-    list =[]
+    a = 1103515245 
+    c = 12345
+    m = 32768
+    list =[seed]
+    for _ in range(9999):
+      seed = (a*seed +c)%m
+      list.append(seed)
+    return list
+      
+    
     
