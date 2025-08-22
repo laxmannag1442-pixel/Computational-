3f826e04dfa6bef3498e3c595b74a24607dcea0e
laxmannag1442-pixel <laxmannag1442@gmail.com>
2025-08-22 18:09:27 +0530

    Update assignment_3.py

--- assignment_3.py
+++ assignment_3.py
@@ -3,7 +3,7 @@
 # name:Laxman Nag , Roll no.: 2311092
 
 
-def lu_decomposition(matrix):
+def lu_decomposition_doolittle(matrix):
   n = len(matrix)
   l=[[0]*n for i in range (n)]
   u=[[0]*n for i in range(n)]
@@ -26,3 +26,22 @@ def lu_decomposition(matrix):
         l[i][j]=(matrix[i][j]-sum_l)/u[j][j]
 
   return l , u               #return lower triangular matrix and upper triangular matrix
+
+
+from lib  import laxmanlibrary
+z = laxmanlibrary()
+A = z.read_matrix("matrix_A")
+l,u = lu_decomposition_doolittle(A)
+
+print("the given matrix :",A)
+print("the lower triangular matrix :",l)
+print("upper triangular matrix :",u)
+print("the verified matrix after matrix multiplication: ",z.matrix_multiplication(l,u))
+
+
+#- - -- -- - -- -- - ---- - -- - THE OUPUT RESULTS - --  -- -  - -  - - -- - -- ------------
+
+# the given matrix : [[1, 2, 4], [3, 8, 14], [2, 6, 13]]
+# the lower triangular matrix : [[1, 0, 0], [3.0, 1, 0], [2.0, 1.0, 1]]
+# upper triangular matrix : [[1, 2, 4], [0, 2.0, 2.0], [0, 0, 3.0]]
+# the verified matrix after matrix multiplication:  [[1, 2.0, 4.0], [3.0, 8.0, 14.0], [2.0, 6.0, 13.0]]
